d552a5e9cba2024e6c26a19fe32df59865f94c42
christine <1059594713@qq.com>
2023-10-23 16:43:48 +0800

    oyster，feat：Init add project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chris
+ * @description 启动入口
+ * @Copyright
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,9 @@
+package cn.bugstack.chatbot.api.test;
+
+/**
+ * @author chris
+ * @description 单元测试
+ * @Copyright
+ */
+public class ApiTest {
+}
